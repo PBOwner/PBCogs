82e2033e3d1f367660d569741a189ac3e22b7d79
PBOwner <165766495+PBOwner@users.noreply.github.com>
2024-04-02 07:29:07 -0400

    Delete pbcogs/modmail directory

--- pbcogs/modmail/__init__.py
+++
@@ -1,7 +0,0 @@
-import discord
-from discord.ext import commands
-from .modmail import ModMail
-
-async def setup(bot):
-    await bot.add_cog(ModMail(bot))
-    print("ModMail cog loaded successfully!")
