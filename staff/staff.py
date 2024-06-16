2c3bde21365bb303bcef47e7a5055c123c78dd3d
PBOwner <165766495+PBOwner@users.noreply.github.com>
2024-06-16 11:21:25 -0400

    Update staff.py

--- staff/staff.py
+++ staff/staff.py
@@ -98,7 +98,7 @@ class StaffManager(commands.Cog):
         embed.add_field(name="User ID", value=member.id, inline=False)
         embed.add_field(name="Reason", value=reason, inline=False)
         embed.add_field(name="Proof", value=proof, inline=False)
-        embed.set_author(name=ctx.author.display_name, icon_url=ctx.author.avatar_url_as(format='png'))
+        embed.set_author(name=ctx.author.name, icon_url=ctx.author.avatar_url)
         await self.blacklist_channel.send(embed=embed)
 
 def setup(bot):
