0d8f630c1fde8c53af598b97d3ea9f21610fd501
PBOwner <165766495+PBOwner@users.noreply.github.com>
2024-06-18 13:46:36 -0400

    Update modmail.py

--- modmail/modmail.py
+++ modmail/modmail.py
@@ -1,163 +1,62 @@
-import discord
-from discord.ext import commands
-from redbot.core import Config
-import asyncio
 from redbot.core import commands
+from redbot.core.bot import Config
+import discord
+import logging
+import datetime
+from discord.ext import commands as Commands
+
+from .thread import Thread
 
 class Modmail(commands.Cog):
+    """Simple modmail cog"""
+
     def __init__(self, bot):
         self.bot = bot
-        self.config = Config.get_conf(self, identifier=1234567890)  # Unique identifier for the config
-    default_guild = {
-        "modmail_channel_id": None,
-        "modmail_category_id": None,
-        "snippets": {}
-}     
-    
+        self.guild_id = 1230169850446479370
+        self.config = Config.get_conf(self, identifier=2480948239048209)
+        self.logger = logging.getLogger('red.mahjesticcogs.modmail')
+
+    def get_thread_from_json(self, json):
+        return Thread(json['member_id'], json['messages'], json['created_at'])
+
+    async def init_threads(self):
+        await self.config.guild_from_id(self.guild_id).threads.set([])
+
+    async def create_user_thread(self, member, initial_message, created_at):
+        async with self.config.guild_from_id(self.guild_id).threads() as threads:
+            threads.append(Thread(member.id, [initial_message.id], int(datetime.datetime.utcnow().timestamp())).json())
+
+    async def get_user_thread(self, member):
+        threads = await self.config.guild_from_id(self.guild_id).threads()
+        for thread in threads:
+            if thread['member_id'] == member.id:
+                return thread
+        return None
+
+    @commands.command()
+    async def threads(self, ctx: Commands.Context):
+        threads = await self.config.guild_from_id(self.guild_id).threads()
+        embed = discord.Embed(title="ModMail Threads")
+        for thread_json in threads:
+            thread = self.get_thread_from_json(thread_json)
+            # embed.add_field(name=f"{member.name} ({member_id})", value="Thread")
+        await ctx.send(embed=embed)
+
     @commands.Cog.listener()
     async def on_message(self, message):
-        if message.author == self.bot.user:
+        if message.guild is not None:
             return
-        
-        if message.guild is None:
-            modmail_channel_id = await self.config.guild(message.guild).modmail_channel_id()
-            if modmail_channel_id:
-                modmail_channel = self.bot.get_channel(modmail_channel_id)
-                if modmail_channel and message.channel.id != modmail_channel_id:
-                    await modmail_channel.send(f"**{message.author}** from DMs: {message.content}")
-    
-    @commands.command()
-    async def set_modmail_channel(self, ctx, channel: discord.TextChannel):
-        """Set the modmail channel for receiving messages."""
-        modmail_category_id = await self.config.guild(ctx.guild).modmail_category_id()
-        if modmail_category_id:
-            category = ctx.guild.get_channel(modmail_category_id)
-            if category:
-                overwrites = {
-                    ctx.guild.default_role: discord.PermissionOverwrite(read_messages=False),
-                    ctx.guild.me: discord.PermissionOverwrite(read_messages=True)
-                }
-                new_channel = await category.create_text_channel(name=channel.name, overwrites=overwrites)
-                await self.config.guild(ctx.guild).modmail_channel_id.set(new_channel.id)
-                await ctx.send(f"Modmail channel set to {new_channel.mention}.")
-            else:
-                await ctx.send("Modmail category is not set or invalid.")
-        else:
-            await ctx.send("Modmail category is not set. Please set a modmail category first.")
-        
-    @commands.command()
-    async def setmmcategory(self, ctx, category: discord.CategoryChannel):
-        """Set the modmail category for creating channels."""
-        await self.config.guild(ctx.guild).modmail_category_id.set(category.id)
-        await ctx.send(f"Modmail category set to {category.name}.")
-    
-    @commands.command()
-    async def mmreply(self, ctx, *, reply):
-        """Reply to a modmail message."""
-        modmail_channel_id = await self.config.guild(ctx.guild).modmail_channel_id()
-        if modmail_channel_id:
-            modmail_channel = self.bot.get_channel(modmail_channel_id)
-            if modmail_channel and ctx.channel.id == modmail_channel_id:
-                await modmail_channel.send(f"**{ctx.author}**: {reply}")
-                await ctx.send("Your reply has been sent.")
-            else:
-                await ctx.send("Modmail channel is not set or invalid.")
-        else:
-            await ctx.send("Modmail channel is not set. Please set a modmail channel first.")
-    
-    @commands.command()
-    async def ar(self, ctx, *, reply):
-        """Reply to a modmail message anonymously."""
-        modmail_channel_id = await self.config.guild(ctx.guild).modmail_channel_id()
-        if modmail_channel_id:
-            modmail_channel = self.bot.get_channel(modmail_channel_id)
-            if modmail_channel and ctx.channel.id != modmail_channel_id:
-                embed = discord.Embed(description=reply, color=0x00ff00)
-                await ctx.send("Your anonymous reply has been sent.")
-                await ctx.author.send(embed=embed)
-            else:
-                await ctx.send("You can only use this command in the modmail channel.")
-        else:
-            await ctx.send("Modmail channel is not set. Please set a modmail channel first.")
-    
-    @commands.group()
-    async def snippet(self, ctx):
-        """Manage snippets for modmail."""
-        pass
-    
-    @snippet.command()
-    async def add(self, ctx, name, content):
-        """Add a new snippet."""
-        snippets = await self.config.guild(ctx.guild).snippets()
-        snippets[name] = content
-        await self.config.guild(ctx.guild).snippets.set(snippets)
-        await ctx.send(f"Snippet '{name}' added.")
-    
-    @snippet.command()
-    async def edit(self, ctx, name, content):
-        """Edit an existing snippet."""
-        snippets = await self.config.guild(ctx.guild).snippets()
-        if name in snippets:
-            snippets[name] = content
-            await self.config.guild(ctx.guild).snippets.set(snippets)
-            await ctx.send(f"Snippet '{name}' edited.")
-        else:
-            await ctx.send(f"Snippet '{name}' does not exist.")
-    
-    @snippet.command()
-    async def delete(self, ctx, name):
-        """Delete a snippet."""
-        snippets = await self.config.guild(ctx.guild).snippets()
-        if name in snippets:
-            del snippets[name]
-            await self.config.guild(ctx.guild).snippets.set(snippets)
-            await ctx.send(f"Snippet '{name}' deleted.")
-        else:
-            await ctx.send(f"Snippet '{name}' does not exist.")
-    
-    @commands.command()
-    async def close(self, ctx, reason: str, time_until_close: int):
-        """Close the modmail thread with a reason and specified time until closure."""
-        modmail_channel_id = await self.config.guild(ctx.guild).modmail_channel_id()
-        if modmail_channel_id:
-            modmail_channel = self.bot.get_channel(modmail_channel_id)
-            if modmail_channel and ctx.channel.id != modmail_channel_id:
-                await ctx.send(f"The modmail thread will be closed in {time_until_close} seconds with reason: {reason}")
-                await asyncio.sleep(time_until_close)
-                await modmail_channel.send(f"The modmail thread has been closed. Reason: {reason}")
-                await ctx.channel.delete()
-            else:
-                await ctx.send("You can only use this command in the modmail channel.")
-        else:
-            await ctx.send("Modmail channel is not set. Please set a modmail channel first.")
-    
-    @commands.command()
-    async def renamet(self, ctx, new_name):
-        """Rename the modmail thread."""
-        modmail_channel_id = await self.config.guild(ctx.guild).modmail_channel_id()
-        if modmail_channel_id:
-            modmail_channel = self.bot.get_channel(modmail_channel_id)
-            if modmail_channel and ctx.channel.id != modmail_channel_id:
-                await modmail_channel.edit(name=new_name)
-                await ctx.send(f"The modmail thread has been renamed to '{new_name}'.")
-            else:
-                await ctx.send("You can only use this command in the modmail channel.")
-        else:
-            await ctx.send("Modmail channel is not set. Please set a modmail channel first.")
-    
-    @commands.command()
-    async def movet(self, ctx, category: discord.CategoryChannel):
-        """Move the modmail thread to a different category."""
-        modmail_channel_id = await self.config.guild(ctx.guild).modmail_channel_id()
-        if modmail_channel_id:
-            modmail_channel = self.bot.get_channel(modmail_channel_id)
-            if modmail_channel and ctx.channel.id != modmail_channel_id:
-                await modmail_channel.edit(category=category)
-                await ctx.send(f"The modmail thread has been moved to the category '{category}'.")
-            else:
-                await ctx.send("You can only use this command in the modmail channel.")
-        else:
-            await ctx.send("Modmail channel is not set. Please set a modmail channel first.")
+        if message.author.bot:
+            return
 
-def setup(bot):
-    bot.add_cog(Modmail(bot))
+        threads = await self.config.guild_from_id(self.guild_id).threads()
+        if not threads:
+            await self.init_threads()
+
+        user_thread = await self.get_user_thread(message.author)
+        if not user_thread:
+            await self.create_user_thread(message.author, message, datetime.datetime.now())
+            await message.add_reaction("")
+            await message.channel.send(embed=discord.Embed(title="Thread Created", description="A staff member will be with you shortly."))
+        else:
+            await message.add_reaction("")
