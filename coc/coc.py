8840b87566d9347e850a94e7ba4deaf567a1f323
PBOwner <165766495+PBOwner@users.noreply.github.com>
2024-06-18 21:49:15 -0400

    Update coc.py

--- coc/coc.py
+++ coc/coc.py
@@ -10,6 +10,7 @@ class COC(commands.Cog):
             "roles": []
         }
         self.config.register_guild(**default_guild)
+        self.previous_roles = {}
         self.update_chain_of_command.start()
 
     def cog_unload(self):
@@ -40,23 +41,39 @@ class COC(commands.Cog):
 
         await ctx.send(embed=embed)
 
-    @tasks.loop(minutes=3)
+    @tasks.loop(seconds=15)
     async def update_chain_of_command(self):
         for guild in self.bot.guilds:
             roles = await self.config.guild(guild).roles()
             roles = sorted([guild.get_role(role_id) for role_id in roles if guild.get_role(role_id)], key=lambda r: r.position, reverse=True)
 
-            embed = discord.Embed(title="Chain of Command", color=discord.Color.blue())
+            updated = False
+            current_roles = {}
             for role in roles:
-                members = [member.mention for member in role.members]
-                if members:
-                    embed.add_field(name=role.name, value="\n".join(members), inline=False)
-                else:
-                    embed.add_field(name=role.name, value="No members", inline=False)
+                current_roles[role.id] = {member.id for member in role.members}
 
-            channel = discord.utils.get(guild.text_channels, name="chain-of-command")  # Change this to the specific channel you want to send the embed to
-            if channel:
-                await channel.send(embed=embed)
+            if guild.id not in self.previous_roles:
+                self.previous_roles[guild.id] = {}
+
+            for role_id, members in current_roles.items():
+                if role_id not in self.previous_roles[guild.id] or self.previous_roles[guild.id][role_id] != members:
+                    updated = True
+                    break
+
+            if updated:
+                embed = discord.Embed(title="Chain of Command", color=discord.Color.blue())
+                for role in roles:
+                    members = [member.mention for member in role.members]
+                    if members:
+                        embed.add_field(name=role.name, value="\n".join(members), inline=False)
+                    else:
+                        embed.add_field(name=role.name, value="No members", inline=False)
+
+                channel = discord.utils.get(guild.text_channels, name="chain-of-command")  # Change this to the specific channel you want to send the embed to
+                if channel:
+                    await channel.send(embed=embed)
+
+                self.previous_roles[guild.id] = current_roles
 
     @commands.Cog.listener()
     async def on_member_update(self, before, after):
