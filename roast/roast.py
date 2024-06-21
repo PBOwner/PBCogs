a582a1f1545ef7ffa0c9683947ae6a2a40c2f36a
PBOwner <165766495+PBOwner@users.noreply.github.com>
2024-06-20 21:28:52 -0400

    Update roast.py

--- roast/roast.py
+++ roast/roast.py
@@ -1,19 +1,32 @@
 import aiohttp
+import random
 from redbot.core import commands
 
 class Roast(commands.Cog):
-    """Cog for roasting users using a public API."""
+    """Cog for delivering roasts using a combination of predefined messages and a public API."""
 
     def __init__(self, bot):
         self.bot = bot
         self.session = aiohttp.ClientSession()
+        self.predefined_roasts = [
+            "You're as useless as the 'ueue' in 'queue'.",
+            "If I had a dollar for every time you said something smart, I'd be broke.",
+            "You're not stupid; you just have bad luck thinking.",
+            "You're proof that even god makes mistakes sometimes.",
+            "I'd agree with you, but then we'd both be wrong.",
+            "You bring everyone so much joy when you leave the room.",
+            "You're like a cloud. When you disappear, it's a beautiful day.",
+            "You're the reason why they put instructions on shampoo.",
+            "Your secrets are always safe with me. I never even listen when you tell me them.",
+            "I'd explain it to you, but I left my crayons at home."
+        ]
 
     def cog_unload(self):
         self.bot.loop.create_task(self.session.close())
 
     @commands.command()
     async def roast(self, ctx, user: commands.MemberConverter = None):
-        """Roast a user or yourself if no user is mentioned."""
+        """Deliver a roast to a user or yourself if no user is mentioned."""
         if user is None:
             user = ctx.author
 
@@ -24,7 +37,14 @@ class Roast(commands.Cog):
             await ctx.send("Couldn't fetch a roast at the moment. Try again later.")
 
     async def get_roast(self):
-        """Fetch a roast from a public API."""
+        """Fetch a roast from a predefined list or a public API."""
+        if random.choice([True, False]):
+            return random.choice(self.predefined_roasts)
+        else:
+            return await self.fetch_roast_from_api()
+
+    async def fetch_roast_from_api(self):
+        """Fetch a roast from the evilinsult.com API."""
         url = "https://evilinsult.com/generate_insult.php?lang=en&type=json"
 
         async with self.session.get(url) as response:
