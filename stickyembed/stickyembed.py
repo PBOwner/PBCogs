f1614d9bb3c4bffe2d10dc42a8e2a41f887c9ca6
PBOwner <165766495+PBOwner@users.noreply.github.com>
2024-06-30 23:22:40 -0400

    Update stickyembed.py

--- stickyembed/stickyembed.py
+++ stickyembed/stickyembed.py
@@ -9,6 +9,7 @@ class StickyEmbed(commands.Cog):
         self.sticky_message = None
         self.sticky_channel = None
         self.embed = None
+        self.stop = False
 
     @commands.command()
     async def esticky(self, ctx, title: str, description: str, color: discord.Color, footer: str = None, *fields: str):
@@ -40,13 +41,21 @@ class StickyEmbed(commands.Cog):
         # Store the embed and channel for later use
         self.embed = embed
         self.sticky_channel = ctx.channel
+        self.stop = False
 
         # Send the initial embed to the channel
         self.sticky_message = await ctx.send(embed=embed)
 
+    @commands.command()
+    async def unesticky(self, ctx):
+        """Stop the sticky embed from updating."""
+        if self.sticky_channel and self.sticky_message:
+            self.stop = True
+            await ctx.send("Sticky embed has been stopped from updating.")
+
     @commands.Cog.listener()
     async def on_message(self, message):
         """Listener to keep the embed at the bottom."""
-        if self.sticky_channel and message.channel == self.sticky_channel and not message.author.bot:
+        if self.sticky_channel and message.channel == self.sticky_channel and not message.author.bot and not self.stop:
             await self.sticky_message.delete()
             self.sticky_message = await self.sticky_channel.send(embed=self.embed)
