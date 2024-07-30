f189ee5d78e8f946d8d4441bd29756ea7b6c4a1b
PBOwner <165766495+PBOwner@users.noreply.github.com>
2024-07-30 03:28:07 -0400

    Update hybridcommands.py

--- hybridcommands/hybridcommands.py
+++ hybridcommands/hybridcommands.py
@@ -1,14 +1,13 @@
 import discord
-from discord import app_commands
 from discord.ext import commands
 from redbot.core import commands as red_commands
+from redbot.core.bot import Red
 
 class HybridCommands(red_commands.Cog):
     """Cog for creating hybrid commands that can be used as slash commands."""
 
-    def __init__(self, bot):
+    def __init__(self, bot: Red):
         self.bot = bot
-        self.tree = app_commands.CommandTree(self.bot)
 
     @red_commands.is_owner()
     @red_commands.command()
@@ -22,8 +21,7 @@ class HybridCommands(red_commands.Cog):
         command_name = self.bot.user.name.lower()
 
         # Define your hybrid command here
-        @self.tree.command(name=command_name, description="Invoke my Wraith! >:D")
-        @app_commands.describe(command="The command to run", arguments="The arguments to pass to the command")
+        @self.bot.tree.command(name=command_name, description="Invoke my Wraith! >:D")
         async def dynamic_command(interaction: discord.Interaction, command: str, arguments: str):
             await interaction.response.send_message(f"Running command: {command} with arguments: {arguments}")
             ctx = await self.bot.get_context(interaction)
@@ -31,4 +29,7 @@ class HybridCommands(red_commands.Cog):
             await self.bot.invoke(ctx)
 
         # Sync the command tree globally
-        await self.tree.sync()
+        await self.bot.tree.sync()
+
+async def setup(bot: Red):
+    await bot.add_cog(HybridCommands(bot))
