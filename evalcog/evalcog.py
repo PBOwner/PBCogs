5a6207919d64dca40b061040f65d937b17516f15
PBOwner <165766495+PBOwner@users.noreply.github.com>
2024-07-30 17:54:39 -0400

    Create evalcog.py

---
+++ evalcog/evalcog.py
@@ -0,0 +1,35 @@
+import discord
+from redbot.core import commands
+from discord.http import Route
+import traceback
+
+class EvalCog(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.is_owner()  # Ensure only the bot owner can run this command
+    @commands.command(name='eval')
+    async def _eval(self, ctx, *, code: str):
+        try:
+            # Prepare the environment for eval
+            env = {
+                'bot': self.bot,
+                'ctx': ctx,
+                'discord': discord,
+                'commands': commands,
+                'Route': Route,
+                '__import__': __import__
+            }
+            # Execute the provided code
+            exec(
+                f'async def __eval_func():\n{textwrap.indent(code, "    ")}',
+                env,
+            )
+            result = await env['__eval_func']()
+            await ctx.send(f'```{result}```')
+        except Exception as e:
+            # Send the traceback in case of an error
+            await ctx.send(f'```{traceback.format_exc()}```')
+
+def setup(bot):
+    bot.add_cog(EvalCog(bot))
