b6c0ae4a52a7939e6002f7c29601efa778b067ec
PBOwner <165766495+PBOwner@users.noreply.github.com>
2024-07-30 17:22:54 -0400

    Update and rename __init__.py to __init__.py

--- slashrouter/__init__.py
+++ slashrouter/__init__.py
@@ -1,6 +1,6 @@
 from redbot.core.bot import Red
 
-from .hybridcommands import SlashRouter
+from .slashrouterimport SlashRouter
 
 async def setup(bot: Red):
     await bot.add_cog(SlashRouter(bot))
