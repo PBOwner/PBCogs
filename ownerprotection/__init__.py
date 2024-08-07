e75e816242d86d71b0a5c8a9be651b2e146ee9f9
PBOwner <165766495+PBOwner@users.noreply.github.com>
2024-08-07 04:55:51 -0400

    Update __init__.py

--- ownerprotection/__init__.py
+++ ownerprotection/__init__.py
@@ -1,10 +1,6 @@
 from redbot.core.bot import Red
 from .ownerprotection import OwnerProtection
 
-__red_end_user_data_statement__ = (
-    "This cog does not store any end user data."
-)
-
 async def setup(bot: Red):
     cog = OwnerProtection(bot)
     await cog.cog_load()
