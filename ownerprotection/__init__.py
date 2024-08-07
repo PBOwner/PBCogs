63360b45c88b37698ed3fcb5b9ab963b823fb10d
PBOwner <165766495+PBOwner@users.noreply.github.com>
2024-08-07 05:29:01 -0400

    Update __init__.py

--- ownerprotection/__init__.py
+++ ownerprotection/__init__.py
@@ -9,9 +9,3 @@ async def setup(bot: Red):
     cog = OwnerProtection(bot)
     await cog.cog_load()
     bot.add_cog(cog)
-    bot.tree.add_command(add_to_protected_owners_list)
-    bot.tree.add_command(remove_from_protected_owners_list)
-
-async def teardown(bot: Red):
-    bot.tree.remove_command("Add to Protected Owners", type=discord.AppCommandType.user)
-    bot.tree.remove_command("Remove from Protected Owners", type=discord.AppCommandType.user)
