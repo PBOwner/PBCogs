8a3f32dfa16184716b332256f6ea233a5727946a
PBOwner <165766495+PBOwner@users.noreply.github.com>
2024-08-07 02:48:41 -0400

    Update __init__.py

--- adwarn/__init__.py
+++ adwarn/__init__.py
@@ -1,6 +1,10 @@
 from redbot.core.bot import Red
+from .adwarn import AdWarn, adwarn_context_menu
 
-from .adwarn import AdWarn
+async def setup(bot: Red):
+    cog = AdWarn(bot)
+    await bot.add_cog(cog)
+    bot.tree.add_command(adwarn_context_menu)
 
-async def setup(bot):
-    await bot.add_cog(AdWarn(bot))
+async def teardown(bot: Red):
+    bot.tree.remove_command("AdWarn", type=discord.AppCommandType.user)
