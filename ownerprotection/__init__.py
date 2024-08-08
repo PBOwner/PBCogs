b7b0da1f7774e2ca9524c44d439e94ed2cc1bd7a
PBOwner <165766495+PBOwner@users.noreply.github.com>
2024-08-07 22:00:05 -0400

    Update __init__.py

--- ownerprotection/__init__.py
+++ ownerprotection/__init__.py
@@ -1,15 +1,10 @@
 import asyncio
 from redbot.core.bot import Red
+from .ownerprotection import OwnerProtection, add_to_protected_owners_list, remove_from_protected_owners_list
 
 async def setup(bot: Red):
-    from .ownerprotection import OwnerProtection
-
-    # Create an instance of the OwnerProtection cog
     cog = OwnerProtection(bot)
-
-    # Add the cog to the bot
     await bot.add_cog(cog)
-
-    # Register the context menu commands
     bot.tree.add_command(add_to_protected_owners_list)
     bot.tree.add_command(remove_from_protected_owners_list)
+    await bot.tree.sync()
