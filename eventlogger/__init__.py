7f1e69ccd85e944249373869ae32a191a3cb8e2c
PBOwner <165766495+PBOwner@users.noreply.github.com>
2024-08-08 01:11:57 -0400

    Update __init__.py

--- eventlogger/__init__.py
+++ eventlogger/__init__.py
@@ -1,9 +1,9 @@
-    from redbot.core.bot import Red
-    from .eventlogger import EventLogger
-    from .dashboard_integration import DashboardIntegration
+from redbot.core.bot import Red
+from .eventlogger import EventLogger
+from .dashboard_integration import DashboardIntegration
 
-    async def setup(bot: Red):
-        cog = EventLogger(bot)
-        await bot.add_cog(cog)
-        dashboard_integration = DashboardIntegration()
-        await bot.add_cog(dashboard_integration)
+async def setup(bot: Red):
+    cog = EventLogger(bot)
+    await bot.add_cog(cog)
+    dashboard_integration = DashboardIntegration()
+    await bot.add_cog(dashboard_integration)
