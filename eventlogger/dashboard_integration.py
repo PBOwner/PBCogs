6db139d6dc654ed57cf62ef462ec7c8b890cd4e8
PBOwner <165766495+PBOwner@users.noreply.github.com>
2024-08-08 01:13:23 -0400

    Update dashboard_integration.py

--- eventlogger/dashboard_integration.py
+++ eventlogger/dashboard_integration.py
@@ -1,45 +1,45 @@
-    from redbot.core import commands
-    from redbot.core.bot import Red
-    import discord
-    import typing
+from redbot.core import commands
+from redbot.core.bot import Red
+import discord
+import typing
 
-    def dashboard_page(*args, **kwargs):
-        def decorator(func: typing.Callable):
-            func.__dashboard_decorator_params__ = (args, kwargs)
-            return func
-        return decorator
+def dashboard_page(*args, **kwargs):
+    def decorator(func: typing.Callable):
+        func.__dashboard_decorator_params__ = (args, kwargs)
+        return func
+    return decorator
 
-    class DashboardIntegration:
-        bot: Red
+class DashboardIntegration:
+    bot: Red
 
-        @commands.Cog.listener()
-        async def on_dashboard_cog_add(self, dashboard_cog: commands.Cog) -> None:
-            dashboard_cog.rpc.third_parties_handler.add_third_party(self)
+    @commands.Cog.listener()
+    async def on_dashboard_cog_add(self, dashboard_cog: commands.Cog) -> None:
+        dashboard_cog.rpc.third_parties_handler.add_third_party(self)
 
-        @dashboard_page(name=None, description="Configure Event Logging", methods=("GET", "POST"), is_owner=True)
-        async def configure_logging(self, user: discord.User, **kwargs) -> typing.Dict[str, typing.Any]:
-            import wtforms
-            class Form(kwargs["Form"]):
-                def __init__(self):
-                    super().__init__(prefix="configure_logging_form_")
-                event: wtforms.StringField = wtforms.StringField("Event:", validators=[wtforms.validators.InputRequired()])
-                channel: wtforms.IntegerField = wtforms.IntegerField("Channel:", validators=[wtforms.validators.InputRequired(), kwargs["DpyObjectConverter"](discord.TextChannel)])
-                submit: wtforms.SubmitField = wtforms.SubmitField("Set Logging Channel")
+    @dashboard_page(name=None, description="Configure Event Logging", methods=("GET", "POST"), is_owner=True)
+    async def configure_logging(self, user: discord.User, **kwargs) -> typing.Dict[str, typing.Any]:
+        import wtforms
+        class Form(kwargs["Form"]):
+            def __init__(self):
+                super().__init__(prefix="configure_logging_form_")
+            event: wtforms.StringField = wtforms.StringField("Event:", validators=[wtforms.validators.InputRequired()])
+            channel: wtforms.IntegerField = wtforms.IntegerField("Channel:", validators=[wtforms.validators.InputRequired(), kwargs["DpyObjectConverter"](discord.TextChannel)])
+            submit: wtforms.SubmitField = wtforms.SubmitField("Set Logging Channel")
 
-            form: Form = Form()
-            if form.validate_on_submit() and await form.validate_dpy_converters():
-                event = form.event.data
-                channel = form.channel.data
-                async with self.bot.get_cog("EventLogger").config.guild(kwargs["guild"]).channels() as channels:
-                    channels[event] = channel.id
-                return {
-                    "status": 0,
-                    "notifications": [{"message": f"Logging channel for {event} set to {channel.mention}", "category": "success"}],
-                    "redirect_url": kwargs["request_url"],
-                }
-
-            source = "{{ form|safe }}"
+        form: Form = Form()
+        if form.validate_on_submit() and await form.validate_dpy_converters():
+            event = form.event.data
+            channel = form.channel.data
+            async with self.bot.get_cog("EventLogger").config.guild(kwargs["guild"]).channels() as channels:
+                channels[event] = channel.id
             return {
                 "status": 0,
-                "web_content": {"source": source, "form": form},
+                "notifications": [{"message": f"Logging channel for {event} set to {channel.mention}", "category": "success"}],
+                "redirect_url": kwargs["request_url"],
             }
+
+        source = "{{ form|safe }}"
+        return {
+            "status": 0,
+            "web_content": {"source": source, "form": form},
+        }
