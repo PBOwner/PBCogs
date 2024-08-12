d6d90e96c07f7f9a294d88968ff47762d7ef4ffe
PBOwner <165766495+PBOwner@users.noreply.github.com>
2024-08-12 11:31:13 -0400

    Update __init__.py

--- adwarn/__init__.py
+++ adwarn/__init__.py
@@ -1,7 +1,6 @@
 from redbot.core.bot import Red
-from .adwarn import AdWarn, adwarn_context_menu
+from .adwarn import AdWarn
 
 async def setup(bot):
     cog = AdWarn(bot)
     await bot.add_cog(cog)
-    bot.tree.add_command(adwarn_context_menu)
